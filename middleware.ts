92d9071b46190f6748a6600651e220b289a49d69
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-10-30 20:26:59 +0200

    updated

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-    matcher: ['/about/:path*', '/dashboard/:path*'],
+    matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
 };
